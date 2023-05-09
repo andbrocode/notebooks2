5434784d96ded9deca62de88c12d763253d3eab8
andbrocode <andbro@tutanota.com>
2023-05-09 17:54:58 +0200

    updates

--- SagnacFrequency/SagnacSpectra_RZ_daily.py
+++ SagnacFrequency/SagnacSpectra_RZ_daily.py
@@ -1,8 +1,8 @@
-
 #!/usr/bin/env python
 # coding: utf-8
 
-# ## Compute Sagnac Frequency
+## _______________________________________________
+# ## Compute sagnac spectra and helicorder of RZ daily and save to archive
 
 
 import os, gc, json
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 
 from obspy import UTCDateTime
 from scipy.signal import welch, periodogram
-from numpy import zeros, argmax, arange, array, linspace, shape
+from numpy import zeros, argmax, arange, array, linspace, shape, nanmean
 from tqdm import tqdm
 from pandas import DataFrame, date_range
 from datetime import datetime, date
@@ -22,37 +22,45 @@ from andbro__cut_frequencies_array import __cut_frequencies_array
 import warnings
 warnings.filterwarnings('ignore')
 
-
+## _______________________________________________
 ## Configuration
 
 config = {}
 
+## select ring laser
 config['ring'] = "Z"
 
+## specify seed for raw sagnac data
 config['seed_raw'] = f"BW.DROMY..FJ{config['ring']}"
+
+## specify seed for rotation rate data
 config['seed_rot'] = f"BW.ROMY.10.BJ{config['ring']}"
 
-config['tbeg'] = "2022-08-01"
-config['tend'] = "2022-08-31"
+## get date time of yesterday
+config['tbeg'] = str((UTCDateTime.now() - 86400).date)
+config['tend'] = config['tbeg']
 
-config['outpath_data'] = f"/import/kilauea-data/sagnac_spectra/R{config['ring']}/data/"
-config['outpath_figs'] = f"/import/kilauea-data/sagnac_spectra/R{config['ring']}/figures/"
-
+## specify output paths
+config['outpath_data'] = f"/import/freenas-ffb-01-data/romy_plots/"
+config['outpath_figs'] = f"/import/freenas-ffb-01-data/romy_plots/"
 
 config['save_plots'] = True
+config['save_data'] = False
 
-config['repository'] = "george"
+config['repository'] = "archive"  ## "george"
 
 config['method'] = "welch" ## "welch" | "periodogram" | multitaper
 
 config['rings'] = {"Z":553, "U":302, "V":448,"W":448}
 
 config['f_expected'] = config['rings'][config['ring']]  ## expected sagnac frequency
+
 config['f_band'] = 10 ## +- frequency band
 
 config['segment_factor'] = 600 ## seconds
 
-## Variables
+
+## Variables for looping
 
 config['threshold'] = -10
 
@@ -68,179 +76,9 @@ config['interval'] = config['loaded_period']
 
 
 
-
+## _______________________________________________
 ## Methods
 
-def __multitaper_estimate(data, fs, n_windows=4, one_sided=True):
-
-    from spectrum import dpss, pmtm
-    from numpy import zeros, arange, linspace
-
-    NN = len(data)
-
-    spectra, weights, eigenvalues = pmtm(data, NW=2.5, k=n_windows, show=False)
-
-    ## average spectra
-    estimate = zeros(len(spectra[0]))
-    for m in range(n_windows):
-        estimate += (abs(spectra[m])**2)
-    estimate /= n_windows
-
-    l = len(estimate)
-    frequencies = linspace(-0.5*fs, 0.5*fs, l)
-
-    if one_sided:
-        f_tmp, psd_tmp = frequencies[int(l/2):], estimate[:int(l/2)]
-    else:
-        f_tmp, psd_tmp = frequencies, estimate
-
-
-    f_max = f_tmp[argmax(psd_tmp)]
-    p_max = max(psd_tmp)
-    h_tmp = __half_width_half_max(psd_tmp)
-
-    return f_tmp, f_max, p_max, h_tmp
-
-
-def __multitaper_periodogram(data, fs, n_windows=4):
-
-    from spectrum import dpss, pmtm
-    from numpy import zeros, arange, linspace
-    from scipy.signal import find_peaks, peak_widths
-
-    [tapers, eigen] = dpss(len(data), 2.5, n_windows)
-
-    f_maxima, p_maxima, hh  = zeros(n_windows), zeros(n_windows), zeros(n_windows)
-
-    for ii in range(n_windows):
-
-        f_tmp, psd_tmp = periodogram(data,
-                                     fs=fs,
-                                     window=tapers[:,ii],
-                                     nfft=None,
-                                     detrend='constant',
-                                     return_onesided=True,
-                                     scaling='density',
-                                     )
-
-        p_maxima[ii] = max(psd_tmp)
-        f_maxima[ii] = f_tmp[argmax(psd_tmp)]
-
-        ## half widths
-        xx = psd_tmp[argmax(psd_tmp)-10:argmax(psd_tmp)+10]
-        peaks, _ = find_peaks(xx)
-        half = peak_widths(xx, peaks, rel_height=0.5)
-        idx = argmax(half[1])
-        hh[ii] = abs(half[3][idx] -half[2][idx])
-
-    f_mean = sum(f_maxima) / n_windows
-    p_mean = sum(p_maxima) / n_windows
-    h_mean = sum(hh) / n_windows
-
-    return f_tmp, f_mean, p_mean, h_mean
-
-
-def __hilbert_frequency_estimator(config, st, fs):
-
-    from scipy.signal import hilbert
-    import numpy as np
-
-    st0 = st.copy()
-
-    f_lower = config['f_expected'] - config['f_band']
-    f_upper = config['f_expected'] + config['f_band']
-
-    ## bandpass with butterworth
-    st0.detrend("demean")
-    st0.taper(0.1)
-    st0.filter("bandpass", freqmin=f_lower, freqmax=f_upper, corners=8, zerophase=True)
-
-    ## estimate instantaneous frequency with hilbert
-    signal = st0[0].data
-
-    analytic_signal = hilbert(signal)
-
-    amplitude_envelope = np.abs(analytic_signal)
-    instantaneous_phase = np.unwrap(np.angle(analytic_signal))
-    instantaneous_frequency = (np.diff(instantaneous_phase) / (2.0*np.pi) * fs)
-
-    ## cut first and last 5% (corrupted)
-
-    dd = int(0.05*len(instantaneous_frequency))
-
-    t = st0[0].times()
-    t1 = st0[0].times()[1:]
-    t2 = t1[dd:-dd]
-
-    t_mid = t[int((len(t))/2)]
-
-    insta_f_cut = instantaneous_frequency[dd:-dd]
-
-    ## averaging
-    insta_f_cut_mean = np.mean(insta_f_cut)
-#     insta_f_cut_mean = np.median(insta_f_cut)
-
-    return t_mid, insta_f_cut_mean, np.mean(amplitude_envelope) ,np.std(insta_f_cut)
-
-
-def __compute(config, st0, starttime, method="hilbert"):
-
-    from scipy.signal import find_peaks, peak_widths, welch, periodogram
-    from numpy import nan, zeros
-
-    NN = config['NN']
-
-    ii = 0
-    n1 = 0
-    n2 = config['dn']
-
-    tt, ff, hh, pp = zeros(NN), zeros(NN), zeros(NN), zeros(NN)
-
-    while n2 <= config['loaded_period']:
-
-        try:
-
-            ## cut stream to chuncks
-            st_tmp = st0.copy().trim(starttime+n1-config['buffer']-config['offset'], starttime+n1+config['dn']+config['buffer']-config['offset'])
-
-            ## get time series from stream
-            times = st_tmp[0].times(reftime=UTCDateTime("2016-01-01T00"))
-
-            ## get sampling rate from stream
-            df = st_tmp[0].stats.sampling_rate
-
-
-            if method == "hilbert":
-                f_tmp, f_max, p_max, h_tmp = __hilbert_frequency_estimator(config, st_tmp, df)
-
-            elif method == "multitaper_periodogram":
-
-                f_tmp, f_max, p_max, h_tmp = __multitaper_perio(st_tmp[0].data, df, n_windows=config['n_windows'])
-
-            elif method == "periodogram":
-
-                f_tmp, f_max, p_max, h_tmp = __periodogram_estimate(st_tmp, df)
-
-            elif method == "multitaper":
-
-                f_tmp, f_max, p_max, h_tmp = __multitaper_estimate(st_tmp[0].data, df, n_windows=config['n_windows'], one_sided=True)
-
-
-            ## append values to arrays
-            tt[ii] = times[int(len(times)/2)]
-            ff[ii] = f_max
-            pp[ii] = p_max
-            hh[ii] = h_tmp
-
-        except:
-            tt[ii], ff[ii], pp[ii], hh[ii] = nan, nan, nan, nan
-#            print(" -> computing failed")
-
-        ii += 1
-        n1 += config['dn']
-        n2 += config['dn']
-
-    return tt, ff, hh, pp
 
 
 def __makeplot_colorlines(config, ff, data, smooth=None):
@@ -389,15 +227,21 @@ def __makeplot_colorlines_and_helicorder(config, ff, data, traces, peaks=None, s
 
 #     norm_st_max = np.max(traces)
     timeaxis = linspace(0, 60, len(traces[0]))
+    
+    ## reverse list to have a downward helicorder
+    traces.reverse()
 
     for m, tr in enumerate(traces):
 
         norm_tr_max = max(tr)
 
-        ax[1].plot(timeaxis, tr/norm_tr_max - nanmean(tr_norm_tr_max) + m, color=cols[m], alpha=0.3)
+        ax[1].plot(timeaxis, tr/norm_tr_max - nanmean(tr/norm_tr_max) + m, color=cols[m], alpha=0.3)
 
     ax[1].set_yticks(linspace(0,23,24))
-    ax[1].set_yticklabels([str(int(tt)).rjust(2,"0")+":00" for tt in linspace(0,23,24)])
+    
+    tck_lbls = [str(int(tt)).rjust(2,"0")+":00" for tt in linspace(0,23,24)]
+    tck_lbls.reverse()
+    ax[1].set_yticklabels(tck_lbls)
 
     ax[1].set_ylim(-1, 24)
 
@@ -521,18 +365,20 @@ def __check_path(path):
 
 
 
-###################################
-### MAIN ##########################
-###################################
+###########################################################
+########################### MAIN ##########################
+###########################################################
 
 def main():
 
 
-    tbeg = date.fromisoformat(config['tbeg'])
-    tend = date.fromisoformat(config['tend'])
+    tbeg = date.fromisoformat(str(config['tbeg']))
+    tend = date.fromisoformat(str(config['tend']))
 
     print(json.dumps(config, indent=4, sort_keys=True))
 
+    
+        
 
     ### ---------------------------------------------
     ## looping days
@@ -549,7 +395,6 @@ def main():
         ### ---------------------------------------------
         ## looping hours
         for hh in tqdm(range(NNN)):
-#        for hh in tqdm(range(2)):
 
             ## define current time window
             dh = hh*config['loaded_period']
@@ -617,8 +462,9 @@ def main():
         output['psds'] = array(psds)
 
         ## store output
-        date_str = str(xdate)[:10].replace("-","")
-        __save_to_pickle(output, f"{config['outpath_data']}R{config['ring']}_{date_str}.pkl")
+        if config['save_data']:
+            date_str = str(xdate)[:10].replace("-","")
+            __save_to_pickle(output, f"{config['outpath_data']}R{config['ring']}_{date_str}.pkl")
 
 
         ## limit frequency range for plotting
@@ -629,17 +475,17 @@ def main():
             print(f" -> failed to cut frequeny range!")
 
         ## Plotting
-        try:
-            colorlines = __makeplot_colorlines(config, ff, array(psds), smooth=None);
-        except Exception as e:
-            print(" -> failed to plot colorlines!")
-            print(e)
+#         try:
+#             colorlines = __makeplot_colorlines(config, ff, array(psds), smooth=None);
+#         except Exception as e:
+#             print(" -> failed to plot colorlines!")
+#             print(e)
 
-        try:
-            colorlines_smooth = __makeplot_colorlines(config, ff, array(psds), smooth=20);
-        except Exception as e:
-            print(" -> failed to plot colorlines smooth!")
-            print(e)
+#         try:
+#             colorlines_smooth = __makeplot_colorlines(config, ff, array(psds), smooth=20);
+#         except Exception as e:
+#             print(" -> failed to plot colorlines smooth!")
+#             print(e)
 
         try:
             colorlines_heli = __makeplot_colorlines_and_helicorder(config, ff, array(psds), traces, peaks=None, smooth=None);
@@ -652,54 +498,56 @@ def main():
 
 
         if config['save_plots']:
+            
+            ## make date string
+            date_str = str(xdate)[:10].replace("-","")
 
+            ## check for subdirectoriese
+            __check_path(f"{config['outpath_figs']}{UTCDateTime(config['tbeg']).year}/")
+            config['outpath_figs'] += f"{UTCDateTime(config['tbeg']).year}/"
+            
+            __check_path(f"{config['outpath_figs']}R{config['ring']}/")
+            config['outpath_figs'] += f"R{config['ring']}/"
+        
+        
+#               ### PLOT 1 -----------------
+#             try:
+#                 outname = f"plot_sagnacspectra_{date_str}_{config['loaded_period']}_colorlines.png"
+
+#                 colorlines.savefig(
+#                                     f"{config['outpath_figs']}{subdir}{outname}",
+#                                     dpi=300, facecolor='w', edgecolor='w', orientation='portrait',
+#                                     format="png", transparent=False, bbox_inches="tight", pad_inches=0.2,
+#                                    )
+#                 print(f" -> saving: {config['outpath_figs']}{subdir}{outname}...")
+#             except Exception as e:
+#                 print(e)
+#                 pass
+
+#               ### PLOT 2 -----------------
+#             try:
+#                 outname = f"plot_sagnacspectra_{date_str}_{config['loaded_period']}_colorlines_smooth.png"
+
+#                 colorlines_smooth.savefig(
+#                                           f"{config['outpath_figs']}{subdir}{outname}",
+#                                           dpi=300, facecolor='w', edgecolor='w', orientation='portrait',
+#                                           format="png", transparent=False, bbox_inches="tight", pad_inches=0.2,
+#                                          )
+#                 print(f" -> saving: {config['outpath_figs']}{subdir}{outname}...")
+#             except Exception as e:
+#                 print(e)
+#                 pass
+
+            ### PLOT 3 -----------------
             try:
-                ### PLOT 1 -----------------
-                outname = f"plot_sagnacspectra_{date_str}_{config['loaded_period']}_colorlines.png"
-                subdir = f"normal/"
-
-                __check_path(f"{config['outpath_figs']}{subdir}")
-
-                colorlines.savefig(
-                                    f"{config['outpath_figs']}{subdir}{outname}",
-                                    dpi=300, facecolor='w', edgecolor='w', orientation='portrait',
-                                    format="png", transparent=False, bbox_inches="tight", pad_inches=0.2,
-                                   )
-                print(f" -> saving: {config['outpath_figs']}{subdir}{outname}...")
-            except Exception as e:
-                print(e)
-                pass
-
-            try:
-                ### PLOT 2 -----------------
-                outname = f"plot_sagnacspectra_{date_str}_{config['loaded_period']}_colorlines_smooth.png"
-                subdir = f"smooth/"
-
-                __check_path(f"{config['outpath_figs']}{subdir}")
-
-                colorlines_smooth.savefig(
-                                          f"{config['outpath_figs']}{subdir}{outname}",
-                                          dpi=300, facecolor='w', edgecolor='w', orientation='portrait',
-                                          format="png", transparent=False, bbox_inches="tight", pad_inches=0.2,
-                                         )
-                print(f" -> saving: {config['outpath_figs']}{subdir}{outname}...")
-            except Exception as e:
-                print(e)
-                pass
-
-            try:
-                ### PLOT 3 -----------------
-                outname = f"plot_sagnacspectra_{date_str}_{config['loaded_period']}_colorlines_helicorder.png"
-                subdir = f"with_helicorder/"
-
-                __check_path(f"{config['outpath_figs']}{subdir}")
+                outname = f"{date_str}_sagnacspectra_R{config['ring']}.png"
 
                 colorlines_heli.savefig(
-                                    f"{config['outpath_figs']}{subdir}{outname}",
+                                    f"{config['outpath_figs']}{outname}",
                                     dpi=300, facecolor='w', edgecolor='w', orientation='portrait',
                                     format="png", transparent=False, bbox_inches="tight", pad_inches=0.2,
                                     )
-                print(f" -> saving: {config['outpath_figs']}{subdir}{outname}...")
+                print(f" -> saving: {config['outpath_figs']}{outname}...")
             except Exception as e:
                 print(e)
                 pass
