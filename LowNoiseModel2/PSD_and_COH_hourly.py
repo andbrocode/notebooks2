f5e4541ad0da36c31a21394a036ceae7eab2cfc9
andbrocode <andbro@tutanota.com>
2023-11-15 18:10:20 +0100

    updates andbro

--- LowNoiseModel2/PSD_and_COH_hourly.py
+++ LowNoiseModel2/PSD_and_COH_hourly.py
@@ -34,6 +34,10 @@ elif os.uname().nodename == 'kilauea':
     root_path = '/home/brotzer/'
     data_path = '/import/kilauea-data/'
     archive_path = '/import/freenas-ffb-01-data/'
+elif os.uname().nodename == 'lin-ffb-01':
+    root_path = '/home/brotzer/'
+    data_path = '/import/kilauea-data/'
+    archive_path = '/import/freenas-ffb-01-data/'
 
 # In[] ___________________________________________________________
 ''' ---- set variables ---- '''
