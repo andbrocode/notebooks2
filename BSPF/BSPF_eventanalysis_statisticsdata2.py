20a2caeefd816b663f34d79223a9ed3cb36c66fd
andbrocode <andbro@tutanota.com>
2023-12-18 18:46:10 +0100

    updates andbro

--- BSPF/BSPF_eventanalysis_statisticsdata2.py
+++ BSPF/BSPF_eventanalysis_statisticsdata2.py
@@ -591,7 +591,7 @@ df_adr_snr = pd.DataFrame(columns=header_adr_snr)
 
 
 # for event in tqdm(config['mseed_files'][:50]):
-for event in tqdm(config['mseed_files'][:10]):
+for event in tqdm(config['mseed_files']):
 
     yy = int(event.replace(".","_").split("_")[1][:4])
     mm = int(event.replace(".","_").split("_")[1][4:6])
