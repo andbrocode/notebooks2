4c3a4ad004f1ae8c8e3a3d7d3d5155de473e78e3
andbrocode <andbro@tutanota.com>
2024-06-03 20:02:14 +0200

    updates andbro

--- VelocityChanges/VC_BAZ_noise.py
+++ VelocityChanges/VC_BAZ_noise.py
@@ -107,13 +107,11 @@ def __get_time_intervals(tbeg, tend, interval_seconds, interval_overlap):
     tbeg, tend = UTCDateTime(tbeg), UTCDateTime(tend)
 
     times = []
-    # t1, t2 = tbeg - interval_overlap, tbeg + interval_seconds + interval_overlap
     t1, t2 = tbeg, tbeg + interval_seconds
-
     while t2 <= tend:
         times.append((t1, t2))
-        t1 = t1 + interval_overlap
-        t2 = t2 + interval_overlap
+        t1 = t1 + interval_seconds - interval_overlap
+        t2 = t2 + interval_seconds - interval_overlap
 
     return times
 
