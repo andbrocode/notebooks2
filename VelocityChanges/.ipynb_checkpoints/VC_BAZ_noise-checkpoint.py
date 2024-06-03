2bc1317f67ae89cc596cb2cdfbc3077055a17d99
andbrocode <andbro@tutanota.com>
2024-06-03 19:53:59 +0200

    updates andbro

--- VelocityChanges/.ipynb_checkpoints/VC_BAZ_noise-checkpoint.py
+++ VelocityChanges/.ipynb_checkpoints/VC_BAZ_noise-checkpoint.py
@@ -98,6 +98,25 @@ def __load_mlti(tbeg, tend, ring, path_to_archive):
 
     return mlti
 
+
+def __get_time_intervals(tbeg, tend, interval_seconds, interval_overlap):
+
+    from obspy import UTCDateTime
+
+    tbeg, tend = UTCDateTime(tbeg), UTCDateTime(tend)
+
+    times = []
+    # t1, t2 = tbeg - interval_overlap, tbeg + interval_seconds + interval_overlap
+    t1, t2 = tbeg, tbeg + interval_seconds
+
+    while t2 <= tend:
+        times.append((t1, t2))
+        t1 = t1 + interval_overlap
+        t2 = t2 + interval_overlap
+
+    return times
+
+
 def main(config):
 
     times = __get_time_intervals(config['tbeg'], config['tend'], interval_seconds=config['interval_seconds'], interval_overlap=0)
