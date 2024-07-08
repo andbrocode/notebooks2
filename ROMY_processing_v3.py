c6aed740fd2da2098461e39469b8807f576ccdb9
brotzercodes <andbro@tutanota.com>
2024-07-08 19:00:18 +0200

    updates brotzer

--- ROMY_processing_v3.py
+++ ROMY_processing_v3.py
@@ -66,7 +66,51 @@ config['t2'] = config['tend']+config['time_offset']
 
 config['Nexpected'] = int((config['t2'] - config['t1']) * config['sampling_rate'])
 
+def __read_sds(path_to_archive, seed, tbeg, tend, data_format="MSEED"):
 
+    '''
+    VARIABLES:
+     - path_to_archive
+     - seed
+     - tbeg, tend
+     - data_format
+
+    DEPENDENCIES:
+     - from obspy.core import UTCDateTime
+     - from obspy.clients.filesystem.sds import Client
+
+    OUTPUT:
+     - stream
+
+    EXAMPLE:
+    >>> st = __read_sds(path_to_archive, seed, tbeg, tend, data_format="MSEED")
+
+    '''
+
+    import os
+    from obspy.core import UTCDateTime, Stream
+    from obspy.clients.filesystem.sds import Client
+
+    tbeg, tend = UTCDateTime(tbeg), UTCDateTime(tend)
+
+    if not os.path.exists(path_to_archive):
+        print(f" -> {path_to_archive} does not exist!")
+        return
+
+    ## separate seed id
+    net, sta, loc, cha = seed.split(".")
+
+    ## define SDS client
+    client = Client(path_to_archive, sds_type='D', format=data_format)
+
+    ## read waveforms
+    try:
+        st = client.get_waveforms(net, sta, loc, cha, tbeg, tend, merge=-1)
+    except:
+        print(f" -> failed to obtain waveforms!")
+        st = Stream()
+
+    return st
 
 def __get_mlti_intervals(mlti_times, time_delta=60):
 
---
+++ RomyMonitor/makeplot_oszi.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+"""
+Spyder Editor
+
+This is a script to capture the screen of RIGOL DS1054Z Oscilloscope
+
+Based on python3
+
+
+__author__ = 'AndBro'
+__year__   = '2022'
+
+"""
+
+# _____________________________________________________________
+'''---- import libraries ----'''
+
+from ds1054z import DS1054Z
+from IPython.display import Image, display
+from datetime import datetime
+from numpy import asarray
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+
+
+# _____________________________________________________________
+''' ---- configurations ---- '''
+
+# define scope IP address
+DS1054Z_IP = "10.153.82.107"
+
+# specify path to save images
+path_to_images = "/import/kilauea-data/HTML_Monitor/figures/"
+
+
+# oscilloscope settings
+#channel1_volt = 1.0  # in volts
+#time_scaling = 0.002 # in seconds
+
+
+# _____________________________________________________________
+'''---- MAIN ----'''
+
+def main():
+
+    dt = datetime.utcnow()
+
+    # initiate scope object
+    scope = DS1054Z(DS1054Z_IP)
+    # scope = DS1054Z(f'TCPIP::{DS1054Z_IP}::INSTR')
+
+    # activate scope
+    scope.run()
+    
+    scope.set_probe_ratio(1, 1) ## set screen ratio
+
+    # take a screenshot
+    # print(" -> taking screenshot...")
+    bmap_scope = scope.display_data
+
+    # stop acquisition
+    scope.stop()
+
+    # display the screentshot
+    #display(Image(bmap_scope))
+    #print(type(bmap_scope))
+    
+    # save image to file
+    with open(path_to_images+f"tmp_html2_oszi.png", "wb") as _img:
+        _img.write(bmap_scope)
+    
+       
+    try:
+        # load image
+        img = mpimg.imread(path_to_images+f"tmp_html2_oszi.png")
+
+        # plot image as figure with time stamp
+        fig = plt.figure()
+        plt.title(str(dt)[:19]+" UTC")
+        plt.imshow(img)
+        plt.axis("off")
+        plt.close();
+
+        # save image with time stamp
+        fig.savefig(path_to_images+f"html2_oszi.png", format="png", dpi=150, bbox_inches='tight');
+
+    except Exception as e:
+        print(f" -> failed to save figure")
+        print(e)
+        pass
+
+if __name__ == "__main__":
+    main()
+
+# End of File
