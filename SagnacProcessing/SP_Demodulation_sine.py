203f9ddc7a66847c339279b5c32c09344de72a10
andbrocode <andbro@tutanota.com>
2024-07-16 18:57:32 +0200

    updates andbro

--- SagnacProcessing/SP_Demodulation_sine.py
+++ SagnacProcessing/SP_Demodulation_sine.py
@@ -44,8 +44,8 @@ config['path_to_archive'] = archive_path+"romy_archive/"
 config['path_to_out_file'] = archive_path+"temp_archive/"
 
 config['t1'] = UTCDateTime("2024-07-11 15:00")
-# config['t2'] = UTCDateTime("2024-07-11 16:00")
-config['t2'] = config['t1'] +86400
+config['t2'] = UTCDateTime("2024-07-11 17:00")
+# config['t2'] = config['t1'] + 86400
 
 
 # V / count  [0.59604645ug  from obsidian]
