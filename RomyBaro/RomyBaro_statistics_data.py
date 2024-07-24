9bf7039a5c33731e82794c1196b3c8d748e20d6d
brotzercodes <andbro@tutanota.com>
2024-07-24 19:09:13 +0200

    updates brotzer

--- RomyBaro/RomyBaro_statistics_data.py
+++ RomyBaro/RomyBaro_statistics_data.py
@@ -22,21 +22,22 @@ from functions.variance_reduction import __variance_reduction
 
 import warnings
 
+
 if os.uname().nodename == 'lighthouse':
     root_path = '/home/andbro/'
     data_path = '/home/andbro/kilauea-data/'
     archive_path = '/home/andbro/freenas/'
-    bay_path = '/home/andbro/bay200/'
+    bay_path = '/home/andbro/ontap-ffb-bay200/'
 elif os.uname().nodename == 'kilauea':
     root_path = '/home/brotzer/'
     data_path = '/import/kilauea-data/'
     archive_path = '/import/freenas-ffb-01-data/'
-    bay_path = '/bay200/'
+    bay_path = '/import/ontap-ffb-bay200/'
 elif os.uname().nodename in ['lin-ffb-01', 'ambrym', 'hochfelln']:
     root_path = '/home/brotzer/'
     data_path = '/import/kilauea-data/'
     archive_path = '/import/freenas-ffb-01-data/'
-    bay_path = '/bay200/'
+    bay_path = '/import/ontap-ffb-bay200/'
 
 
 config = {}
