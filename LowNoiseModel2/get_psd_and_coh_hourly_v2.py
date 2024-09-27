e807ad9e7a1696d7a0f95a36729ca88d5abc3c0d
andbrocode <andbro@tutanota.com>
2024-09-27 19:51:28 +0200

    updates andbro

--- LowNoiseModel2/get_psd_and_coh_hourly_v2.py
+++ LowNoiseModel2/get_psd_and_coh_hourly_v2.py
@@ -529,9 +529,9 @@ def main(config):
         for n, (t1, t2) in enumerate(tqdm(times)):
 
             set_default = False
-            default_f1 = zeros(int(config.get('nperseg'))+2)
-            default_f2 = zeros(int(config.get('nperseg'))+2)
-            default_ff_coh = zeros(int(config.get('nperseg'))+2)
+            default_f1 = zeros(int(config.get('tseconds'))+2)
+            default_f2 = zeros(int(config.get('tseconds'))+2)
+            default_ff_coh = zeros(int(config.get('tseconds'))+2)
 
             # trim streams for current interval
             _st1 = st1.copy().trim(t1, t2, nearest_sample=True)
@@ -545,9 +545,9 @@ def main(config):
                     cohs = zeros([len(times), int(config.get('nperseg')/2)+1])
 
                 elif config['mode'] == "multitaper":
-                    psds1 = zeros([len(times), int(config.get('nperseg'))+2])
-                    psds2 = zeros([len(times), int(config.get('nperseg'))+2])
-                    cohs = zeros([len(times), int(config.get('nperseg'))+2])
+                    psds1 = zeros([len(times), int(config.get('tseconds'))+2])
+                    psds2 = zeros([len(times), int(config.get('tseconds'))+2])
+                    cohs = zeros([len(times), int(config.get('tseconds'))+2])
 
 
             # check length
