82a1a4fa0de21ef5e4f7a0de8c6e368333443220
brotzercodes <andbro@tutanota.com>
2025-01-13 19:10:59 +0100

    updates brotzer

--- GEORG/csv2mseed_v2.py
+++ GEORG/csv2mseed_v2.py
@@ -19,7 +19,7 @@ path_to_data = "/import/kilauea-data/GEORG/tmp/"
 path_to_data_out = "/import/kilauea-data/GEORG/data/"
 
 # filename = "4h_GEORG_Data.csv"
-filename = "Ringlaser26-12_00-00-00_27-12_00-00-00_MEZ.csv"
+# filename = "Ringlaser26-12_00-00-00_27-12_00-00-00_MEZ.csv"
 
 sps = 7000
 
@@ -33,25 +33,28 @@ for n, filename in enumerate(sorted(os.listdir(path_to_data))):
 
     print(n, filename, starttime)
 
-    if n < 0:
+    if n >= 4:
         continue
 
-    # read csv data to dataframe
-    df = read_csv(path_to_data+filename, names=["time", "data"])
+    if n >= 0:
 
-    # create stream
-    st = __get_stream(df['data'].values, seed_code, starttime, sps=sps)
+        # read csv data to dataframe
+        df = read_csv(path_to_data+filename, names=["time", "data"])
 
-    if n != 0:
-        st += __read_sds(path_to_data_out, seed_code, "2024-12-26 00:00", "2024-12-27 00:00")
+        # create stream
+        st = __get_stream(df['data'].values, seed_code, starttime, sps=sps)
 
-    st.merge()
-    print(st)
+        if n != 0:
+            st += __read_sds(path_to_data_out, seed_code, "2024-12-26 00:00", "2024-12-27 00:00")
 
-    # write data as mseed to SDS archive
-    __write_stream_to_sds(st, path_to_data_out)
+        st.merge()
+        print(st)
+
+        # write data as mseed to SDS archive
+        __write_stream_to_sds(st, path_to_data_out)
+
+        del st, df
 
     # update starttime
     starttime = UTCDateTime(starttime) + chunksize / sps
 
-    del st
