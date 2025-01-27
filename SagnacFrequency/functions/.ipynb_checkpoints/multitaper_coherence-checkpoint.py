42a374190cc6a848e9eb5d3c57ff5477c4cdc9fc
andbrocode <andbro@tutanota.com>
2025-01-27 18:20:19 +0100

    updates andbro

--- SagnacFrequency/functions/.ipynb_checkpoints/multitaper_coherence-checkpoint.py
+++ SagnacFrequency/functions/.ipynb_checkpoints/multitaper_coherence-checkpoint.py
@@ -12,7 +12,6 @@ def __multitaper_coherence(dat1, dat2, Tdelta, n_taper=5, time_bandwidth=3.5, me
     _f1, _psd1 = psd_st1.rspec()
     f1, psd1 = _f1.reshape(_f1.size), _psd1.reshape(_psd1.size)
 
-
     psd_st2 = MTSpec(dat2,
                      dt=Tdelta,
                      nw=time_bandwidth,
@@ -24,13 +23,13 @@ def __multitaper_coherence(dat1, dat2, Tdelta, n_taper=5, time_bandwidth=3.5, me
     f2, psd2 = _f2.reshape(_f2.size), _psd2.reshape(_psd2.size)
 
     if psd1.size == psd2.size:
-        Pxy  = MTCross(psd_st1, psd_st2, wl=0.001)
+        Pxy = MTCross(psd_st1, psd_st2, wl=0.001)
         N = Pxy.freq.size
-        ff_coh, coh = Pxy.freq[:,0][:N//2+1], Pxy.cohe[:,0][:N//2+1]
+        ff_coh, coh = Pxy.freq[:, 0][:N//2+1], Pxy.cohe[:, 0][:N//2+1]
     else:
         print(dat1.size, dat2.size, psd1.size, psd2.size)
 
-    ## output dict
+    # output dictionary
     out = {}
     out['ff1'] = f1
     out['ff2'] = f2
