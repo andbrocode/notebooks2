549c26e9b9c28b21de1eae63b9bd3b8a686ec7dd
andbrocode <andbro@tutanota.com>
2025-02-03 18:25:43 +0100

    updates andbro

--- RomyQualityStatus/functions/.ipynb_checkpoints/load_status-checkpoint.py
+++ RomyQualityStatus/functions/.ipynb_checkpoints/load_status-checkpoint.py
@@ -1,4 +1,4 @@
-def __load_status(tbeg, tend, ring, path_to_data):
+def __load_status(tbeg, tend, ring, path_to_data, verbose=False):
 
     from datetime import date
     from pandas import read_pickle, concat, DataFrame, date_range
@@ -11,14 +11,13 @@ def __load_status(tbeg, tend, ring, path_to_data):
     dd1 = date.fromisoformat(str(tbeg.date))
     dd2 = date.fromisoformat(str(tend.date))
 
-    ## dummy
+    # dummy
     def __make_dummy(date):
         NN = 1440
         df_dummy = DataFrame()
         df_dummy['times_utc'] = array([UTCDateTime(date) + _t for _t in arange(30, 86400, 60)])
         for col in ["quality", "fsagnac", "mlti", "ac_threshold", "dc_threshold"]:
-            df_dummy[col]  = ones(NN)*nan
-
+            df_dummy[col] = ones(NN)*nan
         return df_dummy
 
     df = DataFrame()
@@ -28,22 +27,24 @@ def __load_status(tbeg, tend, ring, path_to_data):
         try:
 
             if not isfile(f"{path_to_data}{file}"):
-                print(f" -> no such file: {file}")
+                if verbose:
+                    print(f" -> no such file: {file}")
                 df = concat([df, __make_dummy(dat)])
             else:
                 df0 = read_pickle(path_to_data+file)
                 df = concat([df, df0])
         except:
-            print(f" -> error for {file}")
+            if verbose:
+                print(f" -> error for {file}")
 
     if df.empty:
         print(" -> empty dataframe!")
         return df
 
-    ## trim to defined times
+    # trim to defined times
     df = df[(df.times_utc >= tbeg) & (df.times_utc < tend)]
 
-    ## correct seconds
-    df['times_utc_sec'] = [abs(tbeg - UTCDateTime(_t))  for _t in df['times_utc']]
+    # correct seconds
+    df['times_utc_sec'] = [abs(tbeg - UTCDateTime(_t)) for _t in df['times_utc']]
 
     return df
